7896c4d1a1d9101756d2473591f3e7a14dfa4ee1
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-24 21:16:28 -0400

    Added flask app and gitignore

---
+++ pose_recorder_app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for, abort
+import time
+import os
+
+if not os.path.exists('uploads'):
+    os.mkdir('uploads')
+
+app = Flask(__name__, static_folder='./templates/')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    print(request.form)
+    if request.method == 'POST':
+        print("Logging in ")
+        username = request.form['email']
+        password = request.form['password']
+        # check if the username and password are correct
+        if username == 'admin@admin.com' and password == 'admin':
+            return redirect(url_for('upload_image'))
+        else:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template('index.html', error=error)
+    else:
+        abort(403)
+
+@app.route('/upload_image', methods=['POST', 'GET'])
+def upload_image():
+    if request.method == 'POST':
+        image = request.files['image']
+        print(image)
+        if image:
+            filename = str(time.time())
+            image.save(f'./uploads/{filename}.jpg')
+
+        return redirect(url_for('upload_image'))
+    else:
+        return render_template('upload_screen.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
